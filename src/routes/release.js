28a8e98f39ef7e9a17932d633f22100a3bbc66cb
Joakim Hedlund <contact@joakimhedlund.com>
2020-10-14 22:47:47 +0200

    Verify messages come from Newreleases.io

--- src/routes/release.js
+++ src/routes/release.js
@@ -3,6 +3,7 @@ const {
   GITHUB_USERNAME = '',
   GITHUB_PERSONAL_TOKEN = '',
   GITHUB_REPO = '',
+  NEWRELEASES_SECRET = '',
 } = process.env
 
 const emptyGitHubCommit = require('make-empty-github-commit')
@@ -15,12 +16,26 @@ module.exports = (api) => {
       version,
     } = req.body
 
-    // TODO: should use signature verification described here: https://newreleases.io/webhooks
+    // Basic request validation.
     if (
+      !req.rawHeaders['X-Newreleases-Timestamp'] ||
+      !req.rawHeaders['X-Newreleases-Signature'] ||
       provider !== 'dockerhub' ||
       project !== 'node' ||
       !['10', '12'].includes(version)
     ) return res.status(400).send({ committed: false })
+
+    // Verify the message was sent by newreleases.io by creating
+    // a local signature and comparing it with the one in the headers.
+    // https://newreleases.io/webhooks
+    const messageToSign = `${req.rawHeaders['X-Newreleases-Timestamp']}.${JSON.stringify(req.body)}`
+    const expectedSignature = crypto
+      .createHmac('sha256', NEWRELEASES_SECRET)
+      .update(messageToSign)
+      .digest('hex')
+    if (expectedSignature !== req.rawHeaders['X-Newreleases-Signature']) {
+      return res.status(403).send({ committed: false })
+    }
 
     const { sha } = await emptyGitHubCommit({
       owner: GITHUB_USERNAME,
